3257d93734a572a9a91d41bf2f66d3133140ad85
ctmakro <ctmakro@gmail.com>
2017-04-11 17:05:41 +0800

    added test scene, sync mode worked fine

--- hello.py
+++ hello.py
@@ -17,12 +17,6 @@ except:
     print ('')
     raise
 
-# start vrep in a sub process
-# vrep.exe -gREMOTEAPISERVERSERVICE_PORT_DEBUG_PREENABLESYNC
-# where PORT -> 19997, DEBUG -> FALSE, PREENABLESYNC -> TRUE
-# by default the server will start at 19997,
-# use the -g argument if you want to start the server on a different port.
-
 import subprocess as sp
 
 # the class holding a subprocess instance.
@@ -46,6 +40,8 @@ class instance():
 # class holding a v-rep simulation environment.
 import time, types, math, random
 import inspect, platform
+
+blocking = vrep.simx_opmode_blocking
 class vrepper():
     def __init__(self,port_num=None):
         if port_num is None:
@@ -73,10 +69,17 @@ class vrepper():
         else:
             raise RuntimeError('Current OS not supported by this piece of code.')
 
+        # start V-REP in a sub process
+        # vrep.exe -gREMOTEAPISERVERSERVICE_PORT_DEBUG_PREENABLESYNC
+        # where PORT -> 19997, DEBUG -> FALSE, PREENABLESYNC -> TRUE
+        # by default the server will start at 19997,
+        # use the -g argument if you want to start the server on a different port.
         path_vrep = dir_vrep + 'vrep'
         args = [path_vrep, '-gREMOTEAPISERVERSERVICE_'+str(self.port_num)+'_FALSE_TRUE']
 
+        # instance created but not started.
         self.instance = instance(args)
+
         self.cid = -1
         # clientID of the instance when connected to server,
         # to differentiate between instances in the driver
@@ -108,6 +111,7 @@ class vrepper():
         print('(vrepper)starting an instance of V-REP...')
         self.instance.start()
 
+        # try to connect to V-REP instance via socket
         retries = 0
         while True:
             print ('(vrepper)trying to connect to server on port',self.port_num,'retry:',retries)
@@ -129,18 +133,16 @@ class vrepper():
                     raise RuntimeError('(vrepper)Unable to connect to V-REP after 15 retries.')
 
         # Now try to retrieve data in a blocking fashion (i.e. a service call):
-        res, objs = self.simxGetObjects(
+        objs, = check_ret(self.simxGetObjects(
             vrep.sim_handle_all,
-            vrep.simx_opmode_blocking)
+            blocking))
 
-        if res == vrep.simx_return_ok:
-            print ('(vrepper)Number of objects in the scene: ',len(objs))
-        else:
-            self.end()
-            raise RuntimeError('(vrepper)Remote API function call returned with error code: ',res)
+        print ('(vrepper)Number of objects in the scene: ',len(objs))
 
         # Now send some data to V-REP in a non-blocking fashion:
-        self.simxAddStatusbarMessage('Hello V-REP!', vrep.simx_opmode_oneshot)
+        self.simxAddStatusbarMessage(
+            '(vrepper)Hello V-REP!',
+            vrep.simx_opmode_oneshot)
         print('(vrepper) V-REP instance started, remote API connection created. Everything seems to be ready.')
         self.started = True
         return self
@@ -157,7 +159,108 @@ class vrepper():
         print('(vrepper) everything shut down.')
         return self
 
-venv = vrepper()
-venv.start()
-# time.sleep(30)
-# venv.end()
+    def load_scene(self,fullpathname):
+        print('(vrepper) loading scene from',fullpathname)
+        ret = self.simxLoadScene(fullpathname,
+            0, # assume file is at server side
+            blocking)
+
+        if ret == vrep.simx_return_ok:
+            print('(vrepper) scene successfully loaded')
+            return True
+        else:
+            print('(vrepper) scene loading failure')
+            return False
+
+    def get_object_handle(self,name):
+        handle, = check_ret(self.simxGetObjectHandle(name,blocking))
+        return handle
+
+    def get_object_by_handle(self,handle):
+        return vrepobject(self,handle)
+
+    def get_object_by_name(self,name):
+        return self.get_object_by_handle(self.get_object_handle(name))
+
+# check return tuple, raise error if retcode is not OK,
+# return remaining data otherwise
+def check_ret(ret_tuple):
+    istuple = isinstance(ret_tuple,tuple)
+    if not istuple:
+        ret = ret_tuple
+    else:
+        ret = ret_tuple[0]
+    if ret!=vrep.simx_return_ok:
+        raise RuntimeError('retcode not OK, API call failed')
+
+    return ret_tuple[1:] if istuple else None
+
+class vrepobject():
+    def __init__(self,env,handle):
+        self.env = env
+        self.handle = handle
+
+    def get_orientation(self, relative_to=None):
+        eulerAngles, = check_ret(self.env.simxGetObjectOrientation(
+            self.handle,
+            -1 if relative_to is None else relative_to.handle,
+            blocking))
+        return eulerAngles
+
+    def get_position(self, relative_to=None):
+        position, = check_ret(self.env.simxGetObjectPosition(
+            self.handle,
+            -1 if relative_to is None else relative_to.handle,
+            blocking))
+        return position
+
+    def get_velocity(self):
+        return check_ret(self.env.simxGetObjectVelocity(
+            self.handle,
+            # -1 if relative_to is None else relative_to.handle,
+            blocking))
+        # linearVel, angularVel
+
+    def read_force_sensor(self):
+        state,forceVector,torqueVector = check_ret(self.env.simxReadForceSensor(
+            self.handle,
+            blocking))
+
+        if state & 1 == 1:
+            return None # sensor data not ready
+        else:
+            return forceVector, torqueVector
+
+if __name__ == '__main__':
+    import os
+
+    venv = vrepper()
+    venv.start()
+
+    # load scene
+    if not venv.load_scene(os.getcwd() + '/scenes/body_joint_wheel.ttt'):
+        venv.end()
+
+    body = venv.get_object_by_name('body')
+    wheel = venv.get_object_by_name('wheel')
+
+    print(body.handle)
+    print(wheel.handle)
+
+    check_ret(venv.simxSynchronous(True))
+    check_ret(venv.simxStartSimulation(blocking))
+
+    for i in range(100):
+        print('simulation step',i)
+        check_ret(venv.simxSynchronousTrigger())
+        print(body.get_position())
+        print(wheel.get_orientation())
+        time.sleep(.01)
+
+    # stop the simulation and reset the scene:
+    check_ret(venv.simxStopSimulation(blocking))
+    check_ret(venv.simxSynchronous(False))
+
+    print('simulation ended.')
+    time.sleep(10)
+    venv.end()
